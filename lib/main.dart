5cd61892a7ebdb9eb8a7122c6c945d971b24590f
Marcin Miszewski <marcin.miszewsky@gmail.com>
2023-05-20 15:16:59 +0200

    Implement int function instead of bool. New colors

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Losowanie',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: const FirstPage(),
     );
@@ -38,8 +38,8 @@ class FirstPage extends StatelessWidget {
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
             colors: [
-              Color.fromARGB(255, 219, 147, 99),
-              Color.fromARGB(255, 105, 255, 85),
+              Color.fromARGB(255, 28, 28, 31),
+              Color.fromARGB(255, 146, 157, 160),
             ],
           ),
         ),
@@ -48,11 +48,11 @@ class FirstPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'CO DZISIAJ ROBIMY?',
+                'Rzuć kością',
                 style: GoogleFonts.anton(
                   fontSize: 30,
                   letterSpacing: 5,
-                  color: Colors.blue,
+                  color: Colors.red,
                 ),
               ),
               const SizedBox(height: 100),
@@ -82,13 +82,10 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final random = Random();
-    final result = random.nextBool();
+    final result = random.nextInt(2);
     return Scaffold(
       appBar: AppBar(
         title: const Text('ODPOWIEDŹ'),
-        backgroundColor: result == true
-            ? Colors.green
-            : const Color.fromARGB(255, 244, 130, 54),
       ),
       body: Container(
         decoration: const BoxDecoration(
@@ -105,45 +102,46 @@ class SecondPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              if (result == true)
-                Text(
-                  'NAUKA',
-                  style: GoogleFonts.anton(
-                    fontSize: 60,
-                    letterSpacing: 10,
-                    color: Colors.green,
-                  ),
+              if (result == 0)
+                Column(
+                  children: [
+                    Text(
+                      'NAUKA',
+                      style: GoogleFonts.anton(
+                        fontSize: 60,
+                        letterSpacing: 10,
+                        color: Colors.red,
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    const CircleAvatar(
+                      backgroundImage: AssetImage('images/study.jpg'),
+                      radius: 120,
+                    ),
+                  ],
                 ),
-              const SizedBox(height: 50),
-              if (result == true)
-                const CircleAvatar(
-                  backgroundImage: AssetImage('images/study.jpg'),
-                  radius: 120,
+              if (result == 1)
+                Column(
+                  children: [
+                    Text(
+                      'Trening',
+                      style: GoogleFonts.anton(
+                        fontSize: 60,
+                        letterSpacing: 10,
+                        color: Colors.red,
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    const CircleAvatar(
+                      backgroundImage: AssetImage('images/training.jpg'),
+                      radius: 120,
+                    ),
+                  ],
                 ),
-              if (result == false)
-                Text(
-                  'TRENING',
-                  style: GoogleFonts.anton(
-                    fontSize: 60,
-                    letterSpacing: 10,
-                    color: const Color.fromARGB(255, 244, 130, 54),
-                  ),
-                ),
-              const SizedBox(height: 50),
-              if (result == false)
-                const CircleAvatar(
-                  backgroundImage: AssetImage('images/training.jpg'),
-                  radius: 120,
-                ),
-              if (result == false)
-                const SizedBox(
-                  height: 50,
-                ),
+              const SizedBox(
+                height: 50,
+              ),
               ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: result == true
-                        ? Colors.green
-                        : const Color.fromARGB(255, 244, 130, 54)),
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
